2a49af1a6c6449e966dadd8f46727365b765bfee
James <james.jolly@okstate.edu>
2025-04-10 20:04:10 -0500

    Initial Commit

---
+++ GraduateAssignment/GraduateAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraduateAssignmentApp.swift
+//  GraduateAssignment
+//
+//  Created by James Jolly on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GraduateAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraduateAssignmentTests/GraduateAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  GraduateAssignmentTests.swift
+//  GraduateAssignmentTests
+//
+//  Created by James Jolly on 4/10/25.
+//
+
+import Testing
+@testable import GraduateAssignment
+
+struct GraduateAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
